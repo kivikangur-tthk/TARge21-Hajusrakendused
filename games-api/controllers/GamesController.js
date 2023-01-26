397d82f04db6db4d2a00f448d2b1b56ec0cefed9
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-01-26 17:43:46 +0200

    Start using seqelize to access database

--- games-api/controllers/GamesController.js
+++ games-api/controllers/GamesController.js
@@ -1,21 +1,7 @@
-const mariadb = require("mariadb")
-const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    connectionLimit: 5
-})
+const db = require("../db")
+const Game = db.games
 
 exports.getAll = async (req, res) => {
-    let connection
-    try {
-        connection = await pool.getConnection()
-        const rows = await connection.query("SELECT id, name FROM games")
-        res.send(rows)
-    } catch (error) {
-        throw error
-    } finally {
-        if (connection) return connection.end()
-    }
+    const games = await Game.findAll({attributes:["name"]})
+    res.send(games)
 }
---
+++ games-api/models/Game.model.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize,Sequelize) => {
+    const Game = sequelize.define("game", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    })
+
+    return Game
+}
