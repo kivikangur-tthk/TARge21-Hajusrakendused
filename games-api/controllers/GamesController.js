6c56fc9621019a14c0e9d159a092d430fe32c77a
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-02-02 17:07:48 +0200

    Add code to indicate successful game creation

--- games-api/controllers/GamesController.js
+++ games-api/controllers/GamesController.js
@@ -1,21 +1,31 @@
-const { db } = require("../db")
-const Game = db.games
+const { db } = require("../db");
+const Game = db.games;
 
 exports.getAll = async (req, res) => {
-    const games = await Game.findAll({attributes:["name"]})
-    res.send(games)
-}
+  const games = await Game.findAll({ attributes: ["name"] });
+  res.send(games);
+};
 
-exports.getById = async (req,res) => {
-    const games = await Game.findByPk(req.params.id)
-    if (games === null) {
-        res.status(404).send({"error":"Game not found"})
-        return
-    }
-    res.send(games)
-}
+exports.getById = async (req, res) => {
+  const games = await Game.findByPk(req.params.id);
+  if (games === null) {
+    res.status(404).send({ error: "Game not found" });
+    return;
+  }
+  res.send(games);
+};
 
-exports.createNew = async (req,res) => {    
-    const game = await Game.create(req.body)
-    res.send(game)
-}
+exports.createNew = async (req, res) => {
+  const game = await Game.create(req.body);
+  res
+    .status(201)
+    .location(`${getBaseUrl(req)}/games/${game.id}`)
+    .json(game);
+};
+
+getBaseUrl = (request) => {
+  return (
+    (request.connection && request.connection.encrypted ? "https" : "http") +
+    `://${request.headers.host}`
+  );
+};
