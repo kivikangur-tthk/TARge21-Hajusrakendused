0fff3d97f9c2383c56ad8137d9c52dd0b01a67dd
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-02-02 14:13:59 +0200

    Add error 404 to endpoint [delivers #184268073]

--- games-api/controllers/GamesController.js
+++ games-api/controllers/GamesController.js
@@ -8,5 +8,9 @@ exports.getAll = async (req, res) => {
 
 exports.getById = async (req,res) => {
     const games = await Game.findByPk(req.params.id)
+    if (games === null) {
+        res.status(404).send({"error":"Game not found"})
+        return
+    }
     res.send(games)
 }
